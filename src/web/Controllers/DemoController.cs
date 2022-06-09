a8ed31fdeaecb5a1f4e20361380bdca48597e5e3
zizhong zhang <zizhong.zhang@caterxpress.com>
2022-06-09 20:31:31 +1000

    External host configurable

--- src/web/Controllers/DemoController.cs
+++ src/web/Controllers/DemoController.cs
@@ -7,15 +7,17 @@ namespace web.Controllers;
 public class DemoController : ControllerBase
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public DemoController(ILogger<HomeController> logger)
+    public DemoController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public async Task<IActionResult> Demo()
     {
-        var client = new HttpClient() { BaseAddress = new Uri("https://localhost:7240") };
+        var client = new HttpClient() { BaseAddress = new Uri(_configuration["ExternalAPIHost"]) };
         var address = await client.GetStringAsync("api/address");
         return Ok(address);
     }
