765b2007934106df60e532e1caf78f16fc43c481
zizhong zhang <zizhong.zhang@caterxpress.com>
2022-07-07 12:35:10 +1000

    Azure AD authentication for app insights

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,4 +1,7 @@
 using api;
+using Azure.Identity;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,7 +20,15 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<ContactContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("ContactDatabase")));
 
-builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.Configure<TelemetryConfiguration>(config =>
+{
+    var credential = new ManagedIdentityCredential();
+    config.SetAzureTokenCredential(credential);
+});
+builder.Services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
+{
+    ConnectionString = "InstrumentationKey=2d1cd380-8ff5-455a-a977-3ef0c67cc322;IngestionEndpoint=https://australiasoutheast-0.in.applicationinsights.azure.com/;LiveEndpoint=https://australiasoutheast.livediagnostics.monitor.azure.com/"
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
